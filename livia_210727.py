78645a507cac3bf611df3343ec45df2dc0eb4abe
liviabsjanchevis <88117484+liviabsjanchevis@users.noreply.github.com>
2021-07-28 21:11:32 -0300

    Add files via upload

---
+++ livia_210727.py
@@ -0,0 +1,18 @@
+#Question 2
+words=["bike","sunglasses","backpack","book","door","sky"]
+
+#Question 3
+for livia in words:
+    print(livia)
+
+#Question 4
+for livia in words:
+    print(livia.upper())
+
+#Question 5
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+#Question 6
+for pink in numbers:
+    print(pink)
+    list(range(1,2,3,4,5,6,7,8,9,10))
